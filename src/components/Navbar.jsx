fec8336bf8318a5cc762e6c3c74313551c2c0950
Vaibhav yeole <vaibhav.yeole@borderfree.io>
2023-09-09 14:01:44 +0530

    test home navigation changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,7 +66,7 @@ const Navbar = () => {
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
-          to="/"
+          to="/portfolio/"
           className="flex items-center gap-2"
           onClick={() => {
             setActive("");
